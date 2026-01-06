b89407d550e160184ac8b958ceb64fb69474bd7e
rishu <rrishu212@gmail.com>
2026-01-06 18:54:41 +0530

    Code SetUp

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+
+
+export const signup = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const login = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const logout = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+
+process.env.PORT = process.env.PORT || 5000;
+
+
+app.use("/api/auth", authRoutes);
+
+
+
+
+app.listen(PORT,()=> console.log('Server is running on port 3000'));
