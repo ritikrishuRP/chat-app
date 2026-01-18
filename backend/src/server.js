2d4fd5dd54637dffc25fcfe54954599c876955de
rishu <rrishu212@gmail.com>
2026-01-18 17:45:15 +0530

    signup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 
-process.env.PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
 
 
 
-app.listen(PORT,()=> console.log('Server is running on port 3000'));
+app.listen(PORT,()=> {
+    console.log(`Server is running on ${3000}`)
+    connectDB();
+});
