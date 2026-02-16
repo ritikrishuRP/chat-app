20c604a60d5ad9b8ba2bfe5e4fddbb30dd559af6
rishu <rrishu212@gmail.com>
2026-02-16 16:09:58 +0530

    arcjet rate limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
@@ -12,6 +12,9 @@ const app = express();
 const PORT = ENV.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
@@ -22,3 +25,11 @@ app.listen(PORT,()=> {
     console.log(`Server is running on ${3000}`)
     connectDB();
 });
+
+
+
+//Rate limiting is way to control how often someone can do something
+//on a website or app like how many times they can refresh a page, make a request to an API, or try to log in.
+
+//protect the server from getting overwhelmed 
+//status code 429 means too many requests, so the server is saying "Hey, slow down! You're making too many requests in a short amount of time. Please wait a bit before trying again."
