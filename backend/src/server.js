da6fd85aefc80ba383b031c30a00a65b94f9d2cf
rishu <rrishu212@gmail.com>
2026-02-17 00:24:54 +0530

    message routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
@@ -17,6 +19,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 
